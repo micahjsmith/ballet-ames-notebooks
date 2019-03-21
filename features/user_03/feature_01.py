01f0c5adc2caafdb566eca7bd142227fcb220c3d
Micah Smith <micahjsmith@gmail.com>
2019-03-21 17:36:29 -0400

    Incorporate changes from Kelvin's updated notebooks

--- features/user_03/feature_01.py
+++ features/user_03/feature_01.py
@@ -1,11 +1,15 @@
 from ballet import Feature
 from ballet.eng import SimpleFunctionTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 """Zeros in 020-090 get cut off. This feature prepends them back."""
 
 input = 'MS SubClass'
 def pad_zero(ser):
     ser.astype(str).str.pad(3, side='left', fillchar='0')
-transformer = SimpleFunctionTransformer(pad_zero)
+transformer = [
+    SimpleFunctionTransformer(pad_zero),
+    OneHotEncoder(),
+]
 name = 'Pad zeros in MS SubClass'
 feature = Feature(input=input, transformer=transformer, name=name)
--- features/user_04/feature_02.py
+++ features/user_04/feature_02.py
@@ -1,9 +1,12 @@
 from ballet import Feature
-from ballet.eng import SimpleFunctionTransformer
+from ballet.eng import NullFiller, SimpleFunctionTransformer
 
 input = ['Yr Sold', 'Year Remod/Add']
 def calc_age(df):
     return df['Yr Sold'] - df['Year Remod/Add']
-transformer = SimpleFunctionTransformer(calc_age)
+transformer = [
+    SimpleFunctionTransformer(calc_age),
+    NullFiller(),
+]
 name = 'Age'
 feature = Feature(input=input, transformer=transformer, name=name)
