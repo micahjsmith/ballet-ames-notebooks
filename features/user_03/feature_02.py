4a87810328260344b44051506ad25d987acb2d2f
kelvin-lu <kelvinlu@mit.edu>
2019-05-20 14:29:50 -0400

    wip analysis

--- features/user_03/feature_02.py
+++ features/user_03/feature_02.py
@@ -5,17 +5,22 @@ from sklearn.preprocessing import OneHotEncoder
 
 input = ['Enclosed Porch', '3Ssn Porch', 'Open Porch SF']
 
+
 def calc_porch_type(df):
     # Porch features
     total_porch_area = df.sum(axis=1)
     porch_type = pd.Series('Unknown', index=df.index)
-    porch_type[(total_porch_area == df['Enclosed Porch']) & (df['Enclosed Porch'] > 0) ] = 'Enclosed'
-    porch_type[(total_porch_area == df['3Ssn Porch']) & (df['3Ssn Porch'] > 0) ] = '3Ssn'
-    porch_type[(total_porch_area == df['Open Porch SF']) & (df['Open Porch SF'] > 0) ] = 'Open'
+    porch_type[(total_porch_area == df['Enclosed Porch'])
+               & (df['Enclosed Porch'] > 0)] = 'Enclosed'
+    porch_type[(total_porch_area == df['3Ssn Porch'])
+               & (df['3Ssn Porch'] > 0)] = '3Ssn'
+    porch_type[(total_porch_area == df['Open Porch SF'])
+               & (df['Open Porch SF'] > 0)] = 'Open'
     return porch_type
 
+
 transformer = [
-    SimpleFunctionTransformer(calc_porch_type)
+    SimpleFunctionTransformer(calc_porch_type),
     OneHotEncoder(),
 ]
 
